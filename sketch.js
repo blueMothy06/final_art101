e8a3dc8d78f3f3174b61621ea2515187c14f2fe6
blueMothy06 <catherine.luong137@gmail.com>
2022-05-01 22:50:10 -0700

    added files

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+let myCanvas;
+
+function preload() {
+
+  myFont = loadFont('AnticSlab-Regular.ttf');
+}
+
+function setup() {
+  myCanvas = createCanvas(400, 400);
+  myCanvas.parent('myCanvas');
+
+
+}
+
+function draw() {
+}
+
+f
