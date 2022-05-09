93187a415acb45a4db6dded1c00f4023fe06c188
blueMothy06 <catherine.luong137@gmail.com>
2022-05-08 23:17:58 -0700

    added screens

--- sketch.js
+++ sketch.js
@@ -1,18 +1,56 @@
-
 let myCanvas;
 
-let homePage;
+let title;
 let endPage;
+let home;
+let results;
 
+let myFont;
+let mode = 'titlePage';
 
 function preload() {
-  myFont = loadFont('Josefin Sans Light.ttf');
+  myFont = loadFont('JosefinSans-Light.ttf');
+  title = loadImage('assets/title.png');
+  endPage = loadImage('assets/end.png');
+  home = loadImage('assets/homePage.png');
+  results = loadImage('assets/results.png');
 }
 
 function setup() {
-  myCanvas = createCanvas(400, 400);
+  myCanvas = createCanvas(600, 600);
   myCanvas.parent('myCanvas');
+  background('white');
 }
 
 function draw() {
+  switch (mode) {
+    case 'titlePage':
+      titlePage();
+      break;
+    case 'ending':
+      ending();
+      break;
+    case 'homePage':
+      homePage();
+      break;
+    case 'searchResults':
+      searchResults();
+      break;
+  }
+}
+
+function titlePage() {
+  background(title);
+}
+
+function ending() {
+  background(endPage);
+}
+
+function homePage() {
+  background(home);
+}
+
+function searchResults() {
+  background(results);
 }
